703978fc14ffc9c9919afa53c2478397a0a07a46
mac <mac@macdeMacBook-Pro.local>
2019-01-14 19:14:23 +0800

    Initial commit

---
+++ src/main/java/com/first/boot/bsdemo/HelloController.java
@@ -0,0 +1,27 @@
+package com.first.boot.bsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Title:
+ *
+ * @Author Li.Wei
+ * @Date 2019/1/14 7:05 PM
+ * @Description:
+ */
+
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String say(){
+        return "Hello World";
+    }
+
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "需授权";
+    }
+}
---
+++ src/test/java/com/first/boot/bsdemo/BsdemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.first.boot.bsdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BsdemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
